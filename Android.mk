532a3d2e020d7d9c62cc14653ed2b2b1ed6aa9a8
Keun Soo Yim <yim@google.com>
2016-07-29 06:46:01 -0700

    build as multi libs and add routine for VTS packaging
    (static lib is deprecated so this's required to test various target
    builds.)
    
    Change-Id: If32e6efc8a58b6c120b851fba52c95bedf709a0c

--- Android.mk
+++ Android.mk
@@ -32,4 +32,8 @@ endif
 endif
 LOCAL_CFLAGS += -Werror
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+
+LOCAL_MULTILIB := both
+LOCAL_COMPATIBILITY_SUITE := vts
 include $(BUILD_SHARED_LIBRARY)
+-include test/vts/tools/build/Android.packaging_sharedlib.mk
