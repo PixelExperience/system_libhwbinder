334305b8251aa5fc289158a4a4d2ec45e60feac6
Steven Moreland <smoreland@google.com>
2016-11-11 12:31:31 -0800

    Update for ::descriptor -> const char*.
    Test: hidl_test/hidl_test_java
    Bug: 32745840
    Change-Id: I3283f40234c945e30f816d250fc30a1c8708de05

--- include/hwbinder/Parcel.h
+++ include/hwbinder/Parcel.h
@@ -72,7 +72,7 @@ public:
     bool                hasFileDescriptors() const;
 
     // Writes the RPC header.
-    status_t            writeInterfaceToken(const String16& interface);
+    status_t            writeInterfaceToken(const char* interface);
 
     // Parses the RPC header, returning true if the interface name
     // in the header matches the expected interface from the caller.
@@ -81,7 +81,7 @@ public:
     // propagating the StrictMode policy mask, populating the current
     // IPCThreadState, which as an optimization may optionally be
     // passed in.
-    bool                enforceInterface(const String16& interface,
+    bool                enforceInterface(const char* interface,
                                          IPCThreadState* threadState = NULL) const;
     bool                checkInterface(IBinder*) const;
 
