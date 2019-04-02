LineageOS/android_frameworks_native

Fix google-explicit-constructor warnings in binder.
* Add explicit keyword to conversion constructors.
Bug: 28341362
Test: build with WITH_TIDY=1

Change-Id: I166ab302b3e842cb6d27de89e85767c459f7d826

include/binder/Binder.h
@@ -80,7 +80,7 @@ private:
 class BpRefBase : public virtual RefBase
 {
 protected:
-                            BpRefBase(const sp<IBinder>& o);
+    explicit                BpRefBase(const sp<IBinder>& o);
     virtual                 ~BpRefBase();
     virtual void            onFirstRef();
     virtual void            onLastStrongRef(const void* id);

PixelExperience/system_libhwbinder

Bug: 129785390

include/hwbinder/Binder.h
@@ -91,7 +91,7 @@ private:
 class BpHwRefBase : public virtual RefBase
 {
 protected:
-                            BpHwRefBase(const sp<IBinder>& o);
+    explicit                BpHwRefBase(const sp<IBinder>& o);
     virtual                 ~BpHwRefBase();
     virtual void            onFirstRef();
     virtual void            onLastStrongRef(const void* id);