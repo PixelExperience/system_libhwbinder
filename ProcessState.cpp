PixelExperience/system_libhwbinder

commit b7e2233a33d84c43c2c13b6f2876754cdbd21a3f

libhwbinder: missing /dev/hwbinder is not a fatal error
b/31458381
b/31240290
causes b/31715232
Test: pass

Change-Id: I9b6b9e8ce5c04e4707f12cf623718a037c35596a
Signed-off-by: Iliyan Malchev <malchev@google.com>

ProcessState.cpp
@@ -353,13 +353,14 @@ ProcessState::ProcessState()
         mVMStart = mmap(0, BINDER_VM_SIZE, PROT_READ, MAP_PRIVATE | MAP_NORESERVE, mDriverFD, 0);
         if (mVMStart == MAP_FAILED) {
             // *sigh*
-            ALOGE("Using /dev/binder failed: unable to mmap transaction memory.\n");
+            ALOGE("Using /dev/hwbinder failed: unable to mmap transaction memory.\n");
             close(mDriverFD);
             mDriverFD = -1;
         }
     }
-
-    LOG_ALWAYS_FATAL_IF(mDriverFD < 0, "Binder driver could not be opened.  Terminating.");
+    else {
+        ALOGE("Binder driver could not be opened.  Terminating.");
+    }
 }
 
 ProcessState::~ProcessState()

commit 1758d138e781fbbefd31409c4ec1859fc4fcdf49

Fix log message in the ProcessState constructor
It is written that we do terminate, but in fact, we do not.
The typo was introduced in the b7e2233a33d84c43c2c13b6f2876754cdbd21a3f
commit.

Test: not necessary.
Change-Id: I5d7206e9284d03a83a137837f1cbdd8c01355e6f
Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

ProcessState.cpp
@@ -455,7 +455,7 @@ ProcessState::ProcessState(size_t mmap_size)
         }
     }
     else {
-        ALOGE("Binder driver could not be opened.  Terminating.");
+        ALOGE("Binder driver could not be opened.");
     }
 }
 